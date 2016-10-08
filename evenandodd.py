5aa96d8b400a4eef4dd05d9dde6485012a9bfd47
shweta singh <shweta singh>
2016-10-08 11:26:51 +0530

    added functions

---
+++ evenandodd.py
@@ -0,0 +1,8 @@
+def isEven(number):
+         if number % 2==0:
+                  print("the number is even")
+         else:
+             print("the number is odd")
+number=int(input("enter a number"))
+
+isEven(number)
